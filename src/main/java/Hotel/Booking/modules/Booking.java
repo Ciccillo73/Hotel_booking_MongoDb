fee85acc195e5f15f1faac3a0e4c1055f808adfb
Francesco <med.dama@gmail.com>
2020-06-30 17:50:24 +0100

    Add variable and constructor

--- src/main/java/Hotel/Booking/modules/Booking.java
+++ src/main/java/Hotel/Booking/modules/Booking.java
@@ -1,4 +1,21 @@
 package Hotel.Booking.modules;
 
+import javax.persistence.Id;
+
 public class Booking {
+
+    @Id
+    public String id;
+
+    public String name;
+
+    public String email;
+
+    public Boolean checked;
+
+    public Booking(String name, String email) {
+        this.name = name;
+        this.email = email;
+        this.checked = false;
+    }
 }
