7459d2a5663f5bdbd311d3b123fd43567f3a491c
Francesco <med.dama@gmail.com>
2020-06-30 18:08:00 +0100

    Adds queries

--- src/main/java/Hotel/Booking/repositories/BookingRepository.java
+++ src/main/java/Hotel/Booking/repositories/BookingRepository.java
@@ -3,5 +3,14 @@ package Hotel.Booking.repositories;
 import Hotel.Booking.modules.Booking;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.List;
+
 public interface BookingRepository extends MongoRepository<Booking, String> {
+
+
+    public List<Booking> findByName(String name);
+    public List<Booking> findByEmail(String email);
+    public List<Booking> findByCheckedTrue();
+    public List<Booking> findByCheckedFalse();
+
 }
